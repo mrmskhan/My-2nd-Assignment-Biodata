79343e23b5447e6c404fa2b683c8c0ffdac8913e
M. SALEEM KHAN <160552274+mrmskhan@users.noreply.github.com>
2024-03-04 03:50:14 +0500

    Add files via upload

---
+++ bio.js
@@ -0,0 +1,81 @@
+// Biodata using ,let, const, comments, string, number, and boolean types
+// Personal Information
+var fullName = "Muhammad Saleem Khan";
+var fatherName = "Muhammad Naseem Khan (Late)";
+var age = 40;
+var email = "maraiaayan20@gmail.com";
+var contact = "0347-2400889";
+var religion = "islam";
+var isStudent = true;
+var isEmplyed = true;
+// Address
+var address = "2A/102, Al-Falah, Society";
+var area = "Shah Faisal Colony";
+var city = "karachi";
+var country = "Pakistan";
+// Computer Skills
+var skills = "Ms Office, Html, Css, Java,";
+var inprocess = "Artificial intelligence, Metaverse, and Web 3.0 Developer & Solopreneur (WMD)";
+// Education
+var matriculation = "PAF Base Faisal School";
+var matriculationFiculty = "science";
+var matriculationYear = 1994;
+var intermediate = "Govt. Superior Science College";
+var intermediateFiculty = "Commerce";
+var intermediateYear = 1996;
+var graduation = "University Of Karachi";
+var graduationFiculty = "Commerce";
+var graduationYear = 1998;
+// Work Experience
+var totalExperienceYear = "20";
+var hasWorkExperience = true;
+var company1 = "Government deptt. (cdgk) Landhi Medical Complex";
+var position1 = "Eye Refrectionist";
+var period1 = "2009 to Till date";
+var company2 = "HS Enterprises";
+var position2 = "Accountant";
+var period2 = "2000 to Till date";
+var company3 = "Telenor Pakistan";
+var position3 = "Cluster";
+var period3 = "2016 to Till date";
+var company4 = " Pakistan";
+var position4 = "Cluster";
+var period4 = "2016 to Till 2023";
+// Printing Biodata
+console.log("Name:                  ", fullName);
+console.log("Father Name:           ", fatherName);
+console.log("Age:                   ", age);
+console.log("Email:                 ", email);
+console.log("Contact:               ", contact);
+console.log("Religion:              ", religion);
+console.log("Is Student:            ", isStudent);
+console.log("Is Employed:           ", isEmplyed);
+console.log("Address:               ", address);
+console.log("Area:                  ", area);
+console.log("City:                  ", city);
+console.log("Country:               ", country);
+console.log("Skills:                ", skills);
+console.log("In Process:            ", inprocess);
+console.log("Metriculation:         ", matriculation);
+console.log("Metriculation Ficulty: ", matriculationFiculty);
+console.log("Metriculation Year:    ", matriculationYear);
+console.log("Intermediate:          ", intermediate);
+console.log("Intermediate Ficulty:  ", intermediateFiculty);
+console.log("Intermediate Year:     ", intermediateYear);
+console.log("Graduation:            ", graduation);
+console.log("Graduation Ficulty:    ", graduationFiculty);
+console.log("Graduation Year:       ", graduationYear);
+console.log("Total Experience Year: ", totalExperienceYear);
+console.log("Has Work Experience:   ", hasWorkExperience);
+console.log("Company1:              ", company1);
+console.log("Position1:             ", position1);
+console.log("Period1:               ", period1);
+console.log("Company2:              ", company2);
+console.log("Position2:             ", position2);
+console.log("Period2:               ", period2);
+console.log("Company3:              ", company3);
+console.log("Position3:             ", position3);
+console.log("Period3:               ", period3);
+console.log("Company4:              ", company4);
+console.log("Position4:             ", position4);
+console.log("Period:                ", period4);
---
+++ bio.ts
@@ -0,0 +1,92 @@
+// Biodata using ,let, const, comments, string, number, and boolean types
+
+// Personal Information
+let fullName: string = "Muhammad Saleem Khan"
+let fatherName: string = "Muhammad Naseem Khan (Late)"
+let age: number = 40
+const email: string = "maraiaayan20@gmail.com"
+const contact: string = "0347-2400889"
+const religion: string = "islam" 
+let isStudent: boolean = true
+let isEmplyed: boolean = true
+
+// Address
+let address: string = "2A/102, Al-Falah, Society"
+let area: string = "Shah Faisal Colony"
+let city: string = "karachi"
+let country: string = "Pakistan"
+
+// Computer Skills
+const skills: string ="Ms Office, Html, Css, Java,"
+let inprocess: string= "Artificial intelligence, Metaverse, and Web 3.0 Developer & Solopreneur (WMD)"
+
+// Education
+const matriculation: string = "PAF Base Faisal School"
+const matriculationFiculty: string = "science"
+const matriculationYear: number = 1994
+const intermediate: string = "Govt. Superior Science College"
+const intermediateFiculty: string = "Commerce"
+const intermediateYear: number = 1996
+const graduation: string = "University Of Karachi"
+const graduationFiculty: string = "Commerce"
+const graduationYear: number = 1998
+
+// Work Experience
+let totalExperienceYear: string = "20"
+const hasWorkExperience: boolean = true
+
+const company1: string = "Government deptt. (cdgk) Landhi Medical Complex"
+const position1: string ="Eye Refrectionist"
+const period1: string = "2009 to Till date"
+
+
+const company2: string = "HS Enterprises"
+const position2: string ="Accountant"
+const period2: string = "2000 to Till date"
+
+const company3: string = "Telenor Pakistan"
+const position3: string ="Cluster"
+const period3: string = "2016 to Till date"
+
+const company4: string = " Pakistan"
+const position4: string ="Cluster"
+const period4: string = "2016 to Till 2023"
+
+// Printing Biodata
+console.log("Name:                  ", fullName)
+console.log("Father Name:           ", fatherName)
+console.log("Age:                   ", age)
+console.log("Email:                 ", email)
+console.log("Contact:               ", contact)
+console.log("Religion:              ", religion)
+console.log("Is Student:            ", isStudent)
+console.log("Is Employed:           ", isEmplyed)
+console.log("Address:               ", address)
+console.log("Area:                  ", area)
+console.log("City:                  ", city)
+console.log("Country:               ", country)
+console.log("Skills:                ", skills)
+console.log("In Process:            ", inprocess)
+console.log("Metriculation:         ", matriculation)
+console.log("Metriculation Ficulty: ", matriculationFiculty)
+console.log("Metriculation Year:    ", matriculationYear)
+console.log("Intermediate:          ", intermediate)
+console.log("Intermediate Ficulty:  ", intermediateFiculty)
+console.log("Intermediate Year:     ", intermediateYear)
+console.log("Graduation:            ", graduation)
+console.log("Graduation Ficulty:    ", graduationFiculty)
+console.log("Graduation Year:       ", graduationYear)
+console.log("Total Experience Year: ", totalExperienceYear)
+console.log("Has Work Experience:   ", hasWorkExperience)
+console.log("Company1:              ", company1)
+console.log("Position1:             ", position1)
+console.log("Period1:               ", period1)
+console.log("Company2:              ", company2)
+console.log("Position2:             ", position2)
+console.log("Period2:               ", period2)
+console.log("Company3:              ", company3)
+console.log("Position3:             ", position3)
+console.log("Period3:               ", period3)
+console.log("Company4:              ", company4)
+console.log("Position4:             ", position4)
+console.log("Period:                ", period4)
